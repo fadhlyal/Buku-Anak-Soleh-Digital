28bbd93078eb4833b8970ecb1131d645485c7696
Fadhly Al-farizi <fadhlyalfarizi30@gmail.com>
2025-06-17 08:53:41 +0700

    Refactor: comment for edit page

--- frontend/bukasol/resources/views/teacher/partials/nilai-uji-bacaan-siswa-detail-action-button.blade.php
+++ frontend/bukasol/resources/views/teacher/partials/nilai-uji-bacaan-siswa-detail-action-button.blade.php
@@ -1,6 +1,6 @@
 <div class="container-fluid w-100">
     <div class="d-flex justify-content-center w-100">
-        <!-- Detail Button to Trigger Detail Modal -->
+        <!-- Edit Button to Trigger Edit Page -->
         <a class="btn btn-sm btn-warning py-2 me-2" href="{{ route('teacher.nilai-uji-bacaan-siswa-edit.index', ['id' => $gradeId]) }}">
             <i class="fa-solid fa-pen-to-square"></i>
         </a>
